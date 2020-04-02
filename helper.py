02ab42ff9ce33b48592232fe02228bf4ec232e35
martin.le-morvan@epitech.eu <martin.le-morvan@epitech.eu@epitech.eu>
2020-04-02 12:58:20 +0200

    <feat> <first_push> first push maths

---
+++ helper.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#!-*- utf-8 -*-
+
+def helper():
+
+    print("USAGE")
+    print("   ./109titration file\n")
+    print("DESCRIPTION")
+    print("   file   a csv file containing ”vol;ph” lines")
